dbe50aeeb84c6f0eb4232c5f181f7e3fe567a8e1
PUJAN PANDEY <arghakhanchipujan@gmail.com>
2024-07-16 11:13:25 +0545

    virtual env

--- db.py
+++ db.py
@@ -22,7 +22,50 @@ def db_conn():
     return conn
 conn=db_conn()
 cur=conn.cursor()
-cur.execute('alter table products add column photo varchar(255)')
+cur.execute('''create table customers(
+  customer_id serial primary key,
+  name varchar(255),
+  email varchar(255),
+  password varchar(255),
+  phone_no varchar(255)
+);
+
+  create table orders(
+  order_id integer primary key,
+  customer_id integer,
+  order_date date,
+  total_amount numeric);
+            
+ create table products(
+  product_id serial primary key
+  name varchar(255),
+  price numeric
+  description varchar(255),
+   photo varchar(255),);
+             
+  create table product_list(
+  product_id integer,
+  order_id integer,
+  quantity integer ,
+   foreign_key(product_id) references products(product_id) ,
+    foreign_key(order_id) references orders(order_id) ,                  
+  
+   );  
+ create table products(
+  product_id integer
+  product_id integer,
+  customer_id integer,
+  rating integer,
+  comment text,
+   foreign_key(product_id) references products(product_id) ,
+    foreign_key(customer_id) references customers(customer_id)                 
+                );
+            
+ create table admins(
+   admin_name varchar(255),
+   admin_email varchar(255),
+   admin_password varchar(255)  )           ''') 
+  
 conn.commit()
 cur.close()
 conn.close()
